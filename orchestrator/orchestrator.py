69bce9473617485fda08b95a97329b4932fc3d85
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-04-24 15:53:06 +0200

    Data obtained from FortiGate via REST API

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -3,6 +3,8 @@ from random import random
 from apscheduler.schedulers.background import BackgroundScheduler
 from json import loads
 import grequests
+import requests
+import gevent
 
 app = Flask(__name__)
 
@@ -39,10 +41,7 @@ def push_value_to_list(list, value):
 
 @app.route("/status", methods=['GET'])
 def hello():
-    global data_cpuload_time1
 
-    push_value_to_list(data_fgtload_time1, random() * 100)
-    push_value_to_list(data_fgtload_time2, random() * 100)
     push_value_to_list(data_fgtload_time3, random() * 100)
     push_value_to_list(data_fgtload_time4, random() * 100)
     push_value_to_list(data_fgtload_time5, random() * 100)
@@ -92,22 +91,80 @@ def hello():
     return response
 
 
-def request_cpu_load_from_nodes():
+urls_login = [
+    'http://192.168.122.40/logincheck',
+    'http://192.168.122.40/logincheck'
+]
+fgt_sessions = [requests.Session() for u in urls_login]
 
-    urls = [
+
+def request_cpu_load_from_nodes():
+    # ******************************
+    # Get Values from Hypervisors
+    # ******************************
+
+    urls_hypervisors = [
         'http://10.210.9.130:61208/api/2/cpu',
         'http://10.210.9.130:61208/api/2/cpu',
         'http://10.210.9.130:61208/api/2/cpu',
         'http://10.210.9.130:61208/api/2/cpu'
     ]
 
-    rs = (grequests.get(u) for u in urls)
+    rs = (grequests.get(u) for u in urls_hypervisors)
 
     results = grequests.map(rs)
+
     push_value_to_list(data_cpuload_time1, loads(results[0].content)['total'])
     push_value_to_list(data_cpuload_time2, loads(results[1].content)['total'])
     push_value_to_list(data_cpuload_time3, loads(results[2].content)['total'])
     push_value_to_list(data_cpuload_time4, loads(results[3].content)['total'])
+
+    # ******************************
+    # Get Values from FortiGates
+    # ******************************
+
+    urls_cpu = [
+        'http://192.168.122.40/api/v2/monitor/system/resource/usage?resource=cpu&interval=1-min',
+        'http://192.168.122.40/api/v2/monitor/system/resource/usage?resource=cpu&interval=1-min'
+    ]
+
+    global fgt_sessions
+    global urls_login
+
+    fgt_login_requests = [None] * len(urls_login)
+
+    # First, request CPU data
+
+    fgt_cpu_requests = []
+    for i in range(len(fgt_sessions)):
+        fgt_cpu_requests.append(grequests.get(urls_cpu[i], session=fgt_sessions[i], headers=fgt_sessions[i].headers))
+
+    fgt_cpu_results = grequests.map(fgt_cpu_requests)
+
+    # Check if request failed because of login
+    # If failed, then login
+
+    for i in range(len(fgt_sessions)):
+        if fgt_cpu_results[i].status_code == 401:
+            print("Login into FortiGate's REST API: ", i)
+            fgt_login_requests[i] = grequests.post(urls_login[i],
+                                                   data='username=admin&secretkey=&ajax=1',
+                                                   session=fgt_sessions[i])
+            r = grequests.send(fgt_login_requests[i])
+            gevent.joinall([r])
+
+    # Only if request to get CPU was 200 OK then
+    # get the value and push it to the list
+
+    for i in range(len(fgt_cpu_results)):
+        if fgt_cpu_results[i].status_code == 200:
+            try:
+                push_value_to_list(globals()['data_fgtload_time'+str(i+1)],
+                                   loads(fgt_cpu_results[i].content)['results']['cpu'][0]['current'])
+            except:
+                print("Error getting data from FortiGate:")
+        else:
+            print("Request was not ok:", i)
 
 
 cron = BackgroundScheduler(daemon=True)
