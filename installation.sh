fa19affe3401bc77b09fcc2106212ea0652c0f61
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2018-10-03 12:56:14 +0200

    Use proper libraries, including cybermapper's

--- installation.sh
+++ installation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt install -y python3-flask
+#For this orchestrator
+sudo apt install -y python3-flask python3-pip
+
+#For cybermapper
+sudo apt install -y make build-essential curl python-dev python3-dev python-virtualenv python3-venv libfontconfig1 libfreetype6 expect sshpass npm
 
 pip3 install apscheduler
 pip3 install grequests
